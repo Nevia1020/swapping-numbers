cdbaf069bffa3c6241b713372c9aa6b48b135a01
Nevia1020 <79929396+Nevia1020@users.noreply.github.com>
2021-03-12 11:26:21 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let swapping_numbers = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(7, 8))
+    basic.showString("" + (swapping_numbers))
+    swapping_numbers = 8
+})
+basic.forever(function () {
+	
+})
